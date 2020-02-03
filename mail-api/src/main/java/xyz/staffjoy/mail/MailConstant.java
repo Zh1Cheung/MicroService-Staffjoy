387dd9a098b9f461a812c0a8ed713b51969f7e87
张子恒 <35570187+Zh1Cheung@users.noreply.github.com>
2020-02-03 23:58:33 +0800

    update mail-api
    更新了main文件

---
+++ mail-api/src/main/java/xyz/staffjoy/mail/MailConstant.java
@@ -0,0 +1,18 @@
+package xyz.staffjoy.mail;
+
+/*
+ *
+ * Mail常量
+ *
+ * @Author:Zh1Cheung 945503088@qq.com
+ * @Date: 9:51 2019/12/30
+ *
+ */
+public class MailConstant {
+    public static final String SERVICE_NAME = "email-service";
+
+    public static final String FROM_NAME = "Staffjoy";
+    public static final String FROM = "staffjoy@dm.staffjoy.xyz";
+    public static final String STAFFJOY_EMAIL_SUFFIX = "@jskillcloud.com";
+    public static final String ALIYUN_REGION_ID = "cn-hangzhou";
+}
